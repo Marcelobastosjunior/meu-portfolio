178bb524f9f9f200c539c23cb4525b09c089da2c
Marcelo Bastos Jr <95103548+Marcelobastosjunior@users.noreply.github.com>
2025-01-25 19:57:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   base: "/meu-portfolio",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
